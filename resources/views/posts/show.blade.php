3a2a396f21a181062bf4a39a447cf54b0e701a41
duartecancela <duartecancela@gmail.com>
2021-07-29 16:10:29 +0100

    video 30

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,11 @@
 
 @section('content')
 
-<h1>{{  $post['title'] }}</h1>
-<p>{{ $post['content'] }}</p>
-
+@if($post['is_new'])
+    <div>A new blo post! Using if</div>
+@else
+    <div>Blog post is old! Using else if</div>
+@endif
+    <h1>{{  $post['title'] }}</h1>
+    <p>{{ $post['content'] }}</p>
 @endsection
